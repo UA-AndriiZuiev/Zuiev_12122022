a7f32647261a711980f4df73eb00c1aad41d8b58
AndriiZuiev <zuevandrey1979@gmail.com>
2023-01-10 22:49:48 +0200

    Apply  recomendayions in Operations.java

--- src/ua_hillel/homework_3/Operations.java
+++ src/ua_hillel/homework_3/Operations.java
@@ -1,26 +1,28 @@
 package ua_hillel.homework_3;
+
 import java.util.Scanner;
 
 public class Operations {
     public static void main(String[] args) {
         int number = 5;
-        String oddEven, month;
+        String oddEven;
+        String month;
         Scanner scanner = new Scanner(System.in);
 
-    //-------------------------------------------------//
+        //-------------------------------------------------//
         oddEven = checkOddEven(number);
         System.out.println(oddEven);
         System.out.println("BYE");
-    //-------------------------------------------------//
+        //-------------------------------------------------//
         System.out.println(printMonthInWord_1(number));
-        month = printMonthInWord_2(number+1);
+        month = printMonthInWord_2(number + 1);
         System.out.println(month);
-    //-------------------------------------------------//
+        //-------------------------------------------------//
         System.out.print("Введіть два числа для визначення більшого з них значення - ");
         int number_1 = scanner.nextInt();
         int number_2 = scanner.nextInt();
         magnitude(number_1, number_2);
-    //-------------------------------------------------//
+        //-------------------------------------------------//
         int side_1 = 8;
         int side_2 = 8;
         int side_3 = 8;
@@ -28,43 +30,45 @@ public class Operations {
         triangle(side_1, side_2, side_3);
     }
 
-    public static String checkOddEven(int i){
-        if ((i % 2) == 0)
+    public static String checkOddEven(int i) {
+        if ((i % 2) == 0) {
             return "Even Number";
-        else
+        } else {
             return "Odd Number";
+        }
     }
 
-    public static String printMonthInWord_1(int i){
-        if(i==1)
+    public static String printMonthInWord_1(int i) {
+        if (i == 1) {
             return "JAN";
-        else if(i==2)
-            return  "FEB";
-        else if(i==3)
+        } else if (i == 2) {
+            return "FEB";
+        } else if (i == 3) {
             return "MAR";
-        else if(i==4)
+        } else if (i == 4) {
             return "APR";
-        else if(i==5)
+        } else if (i == 5) {
             return "MAY";
-        else if(i==6)
+        } else if (i == 6) {
             return "JUN";
-        else if(i==7)
+        } else if (i == 7) {
             return "JUL";
-        else if(i==8)
+        } else if (i == 8) {
             return "AUG";
-        else if(i==9)
+        } else if (i == 9) {
             return "SEP";
-        else if(i==10)
+        } else if (i == 10) {
             return "OCT";
-        else if(i==11)
+        } else if (i == 11) {
             return "NOV";
-        else if(i==12)
+        } else if (i == 12) {
             return "DEC";
-        else
+        } else {
             return "Not a valid month";
+        }
     }
 
-    public static String printMonthInWord_2(int i){
+    public static String printMonthInWord_2(int i) {
         switch (i) {
             case 1:
                 return "JAN";
@@ -95,20 +99,21 @@ public class Operations {
         }
     }
 
-    public static void magnitude (int number_1, int number_2){
-        if (Math.abs(number_1) > Math.abs(number_2))
+    public static void magnitude(int number_1, int number_2) {
+        if (Math.abs(number_1) > Math.abs(number_2)) {
             System.out.println("Число " + number_1 + " має велику величину");
-        else if (Math.abs(number_1) < Math.abs(number_2))
+        } else if (Math.abs(number_1) < Math.abs(number_2)) {
             System.out.println("Число " + number_2 + " має велику величину");
-        else
+        } else {
             System.out.println("Числа " + number_1 + " и " + number_2 + " мають однакову величину");
+        }
     }
 
-    public static void triangle(int side_1, int side_2,int side_3){
+    public static void triangle(int side_1, int side_2, int side_3) {
 
-        if(side_1 == side_2 || side_1 == side_3 || side_2 == side_3)
-           System.out.println("Трикутник рівнобедрений");
-        else
-           System.out.println("Трикутник  нерівнобедрений");
+        if (side_1 == side_2 || side_1 == side_3 || side_2 == side_3){
+            System.out.println("Трикутник рівнобедрений");}
+        else{
+            System.out.println("Трикутник  нерівнобедрений");}
     }
 }
